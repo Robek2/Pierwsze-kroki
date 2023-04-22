c7472357d4aecd30530e1d889ab2a87e72187846
Robek2 <robert.kastelik@wp.pl>
2023-04-23 01:19:50 +0200

    Sam chciałbym wiedzieć

---
+++ Pierwsze kroki/Pierwsze kroki/Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("What I can do with that");
+Console.Write("It is so interesting question");
+Console.Write(" ");
+Console.WriteLine("I really don't know");
