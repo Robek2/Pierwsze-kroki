93d6a6e9279623167430da2ce5b8b19543923933
Robek2 <robert.kastelik@wp.pl>
2023-04-29 19:04:52 +0200

    Zadanie dzień 4

---
+++ Zadanie domowe dzień 4/Zadanie domowe dzień 4/Program.cs
@@ -0,0 +1,26 @@
+﻿string name = "Ewa";
+string gender = "Female";
+int age = 33;
+
+if (gender == "Female")
+{
+    if (age < 30)
+    {
+        Console.WriteLine("Kobieta poniżej 30 lat");
+    }
+    else if (age == 33 && name == "Ewa")
+    {
+        Console.WriteLine("Ewa, lat 33");
+    }
+}
+if (gender == "Male")
+{
+    if (age < 18)
+    {
+        Console.WriteLine("Niepełnoletni Mężczyzna");
+    }
+    else if(age >= 18)
+    {
+        Console.WriteLine("Pełnoletni Mężczyzna");
+    }
+}
